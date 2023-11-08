4d8a9f6689b4614f533178819873c44de69f30aa
Emil Boman <nat14ebo@student.lu.se>
2023-11-08 14:49:48 +0100

    Added momentum components and function for calculating momentum magnitudes.

--- Event.cxx
+++ Event.cxx
@@ -1,14 +1,25 @@
 #include "Event.h"
+#include <cmath>
 
 ClassImp(Event)
+
 Event::Event() :
-event_numb{0}
+event_numb{0},
+px{0},
+py{0},
+pz{0}
 {   
 }
-Event::Event(Int_t event_numb) :
-event_numb{event_numb}
+Event::Event(Int_t event_numb, Double_t px, Double_t py, Double_t pz) :
+event_numb{event_numb},
+px{px},
+py{py},
+pz{pz}
 {
+  Magnitude();
 }
 Event::~Event(){
-
+}
+Double_t Event::Magnitude(){
+    return sqrt(pow(px,2)+pow(py,2)+pow(pz,2));
 }
